90effe19ae79941f8e2cd639eeff7ccaa25124b9
KrivosheevEV <euja@yandex.ru>
2017-04-22 13:20:14 +0400

    first versions 1

---
+++ app/src/main/java/ru/kev163rus/thefutureguy2/activity_menu.java
@@ -0,0 +1,13 @@
+package ru.kev163rus.thefutureguy2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
